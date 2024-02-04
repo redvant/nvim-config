d7ee674b18a43526ccb653523bd06478d66d83a7
Edgar Roman Cervantes <redvant@outlook.com>
2024-02-04 14:37:40 -0600

    Change lazy plugings structure

--- lua/red/init.lua
+++ lua/red/init.lua
@@ -11,5 +11,5 @@ require("red.lazy")
 -- hi MatchParen guibg=#5F8787 guifg=#1C1C1C
 
 -- Meant to make bg transparent, not working
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+--vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
--- lua/red/lazy.lua
+++ lua/red/lazy.lua
@@ -11,14 +11,4 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
-}
-local opts = {}
-
-require("lazy").setup(plugins, opts)
+require("lazy").setup("plugins")
