25d145577e7a58dd3800ee3d1cce60b7cc9da1e1
Edgar Roman Cervantes <redvant@outlook.com>
2024-02-05 13:19:54 -0600

    Add settings for gopls

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -9,8 +9,31 @@ vim.opt.expandtab = false
 vim.keymap.set("n", "<leader>gt", function()
     vim.cmd("!go test")
 end)
+
+-- Replaced for BufWritePre and none-ls keymap
+--[[
 vim.keymap.set("n", "<leader>gf", function()
     vim.cmd.write()
     local path = vim.api.nvim_buf_get_name(0)
     vim.cmd("!go fmt " .. path)
 end)
+]]--
+
+-- On Save format with gopls and OrganizeImports(add and organize)
+vim.api.nvim_create_autocmd('BufWritePre', {
+    group = vim.api.nvim_create_augroup('LspGo', {}),
+    callback = function()
+        local params = vim.lsp.util.make_range_params()
+        params.context = {only = {"source.organizeImports"}}
+        local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+        for cid, res in pairs(result or {}) do
+            for _, r in pairs(res.result or {}) do
+                if r.edit then
+                    local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+                    vim.lsp.util.apply_workspace_edit(r.edit, enc)
+                end
+            end
+        end
+        vim.lsp.buf.format()
+    end,
+})
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -8,8 +8,8 @@ return {
     {
         "williamboman/mason-lspconfig.nvim",
         config = function()
-            require("mason-lspconfig").setup{
-                ensure_installed = {"lua_ls", "gopls"}
+            require("mason-lspconfig").setup {
+                ensure_installed = { "lua_ls", "gopls" }
             }
         end
     },
@@ -17,14 +17,24 @@ return {
         "neovim/nvim-lspconfig",
         config = function()
             local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup{}
-            lspconfig.gopls.setup{}
+            lspconfig.lua_ls.setup {}
+            lspconfig.gopls.setup {
+                settings = {
+                    gopls = {
+                        analyses = {
+                            unusedparams = true,
+                        },
+                        staticcheck = true,
+                        gofumpt = true,
+                    },
+                },
+            }
 
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
             vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
-            vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+            vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
         end
     }
 }
