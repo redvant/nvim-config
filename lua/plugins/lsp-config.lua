6e38b1c10a9e5b23ff7035600d34e51a0f779bd3
Edgar Roman Cervantes <redvant@outlook.com>
2024-12-29 23:12:45 -0600

    Add clangd lspconfig

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "gopls" },
+                ensure_installed = { "lua_ls", "gopls", "clangd" },
             })
         end,
     },
@@ -31,6 +31,19 @@ return {
                         staticcheck = true,
                         gofumpt = true,
                     },
+                },
+            })
+            lspconfig.clangd.setup({
+                cmd = {
+                    "clangd",
+                    "--background-index",
+                    "--clang-tidy",
+                    -- "--header-insertion=iwyu",
+                    -- "--completion-style=detailed",
+                    -- "--log=verbose",
+                },
+                init_options = {
+                    -- completeUnimported = true,
                 },
             })
 
