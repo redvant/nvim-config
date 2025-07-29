63e629c801b4b80bcb423e2e16248954c08a9418
Edgar Roman Cervantes <redvant@outlook.com>
2025-07-28 23:57:19 -0600

    Add descriptions to remap keymaps

--- lua/red/remap.lua
+++ lua/red/remap.lua
@@ -1,24 +1,24 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open netrw on current file directory" })
 
 -- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 -- vim.keymap.set("n", "<C-i>", "<cmd>silent !tmux neww cht.sh<CR>")
 
 -- cicle quickfix list
-vim.keymap.set("n", "<C-j>", vim.cmd.cnext)
-vim.keymap.set("n", "<C-k>", vim.cmd.cprev)
+vim.keymap.set("n", "<C-j>", vim.cmd.cnext, { desc = "Next item in quickfix" })
+vim.keymap.set("n", "<C-k>", vim.cmd.cprev, { desc = "Previous item in quickfix" })
 
 -- replace selected with latest register, and send text to replace to nul register
-vim.keymap.set("v", "<leader>p", '"_dp')
+vim.keymap.set("v", "<leader>p", '"_dp', { desc = "Replace selected with latest register, send to null register" })
 -- yank selected to system clipboard
-vim.keymap.set("v", "<leader>y", '"+y')
-vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>y", '"+y', { desc = "Yank selected to system clipboard" })
+-- vim.keymap.set("n", "<leader>y", '"+y')
 -- yank entire file to system clipboard
-vim.keymap.set("n", "<leader>Y", 'gg"+yG')
+vim.keymap.set("n", "<leader>Y", 'gg"+yG', { desc = "Yank entire file to system clipboard" })
 
 -- move selection up or down a line
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down a line" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up a line" })
 
 -- vim.keymap.set("v", "J", function()
 --     vim.cmd.move("'>+1")
